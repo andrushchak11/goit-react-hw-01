269426048e46c598ceae3577486f811a4ca3c94e
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-02-22 14:18:27 +0200

    write profile and start friend list

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
+
+function App() {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,13 @@
+import styles from "./FriendListItem.module.css";
+
+export default function FriendListItem({ friend: avatar, name, isOnline }) {
+  return (
+    <div className={styles.friendListItem}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p style={{ color: isOnline ? "green" : "red" }}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+}
